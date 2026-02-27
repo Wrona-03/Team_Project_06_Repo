ee711e4db01c9642541046827e25e9753658c939
Thitsar Thway <tsthwayy59@gmail.com>
2026-02-27 13:35:18 +0000

    Fixed body parser issue

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const stopsRouter = require('./routes/stopsRoutes');
 
 // Create Express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // For form data
+
 
 app.use(cors());
 app.use(express.static(path.join(__dirname,"public")));
